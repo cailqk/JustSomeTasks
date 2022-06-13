a200bcc173a4d7b6b2ee09c6e50b4a4cfc53b710
cailqk <tsvetan.bogdanov@abv.bg>
2022-06-13 12:46:00 +0300

    task (hex) added

---
+++ hex.js
@@ -0,0 +1,35 @@
+class Hex {
+  constructor(value) {
+    this.value = Number(value);
+  }
+
+  valueOf() {
+    return this.value;
+  }
+
+  toString() {
+      return '0x' + this.value.toString(16).toLocaleUpperCase();
+  }
+
+  plus(num) {
+    return '0x' + (this.value + num).toString(16).toLocaleUpperCase();
+  }
+  
+  minus(num) {
+    return '0x' + (this.value - num).toString(16).toLocaleUpperCase();
+  }
+
+  parse(str) {
+    return parseInt(str, 16);
+  }
+}
+
+let FF = new Hex(255);
+console.log(FF.toString());
+console.log(FF.valueOf() + 1 == 256 );
+let a = new Hex(10);
+let b = new Hex(5);
+console.log(a.plus(b).toString());
+console.log(a.plus(b).toString() === "0xF");
+console.log(FF.parse("AAA"));
+
